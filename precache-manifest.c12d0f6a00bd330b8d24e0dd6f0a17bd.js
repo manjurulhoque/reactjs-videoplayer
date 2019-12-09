7ba901a756e4e622020005d8b575f3f899996242
Rumi <todoyel19@gmail.com>
2019-12-09 14:53:00 +0600

    Updates

---
+++ precache-manifest.c12d0f6a00bd330b8d24e0dd6f0a17bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84126c0808a3bda317d8270f5587ce05",
+    "url": "/react-videoplayer/index.html"
+  },
+  {
+    "revision": "782ae5baf53fa677a1d9",
+    "url": "/react-videoplayer/static/js/2.8d2cc2bb.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/react-videoplayer/static/js/2.8d2cc2bb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f381b59ea040e004394f",
+    "url": "/react-videoplayer/static/js/main.c4668bfa.chunk.js"
+  },
+  {
+    "revision": "48ae8a3c28ba60bdf72b",
+    "url": "/react-videoplayer/static/js/runtime-main.c7657c4f.js"
+  }
+]);
